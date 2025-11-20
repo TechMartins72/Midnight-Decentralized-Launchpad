5fdb7884ae6db67dcd17e3338389241c625f5381
Tech Martins <techmartins72gmail.com>
2025-11-20 02:53:16 +0100

    create statera launchpad template

---
+++ apps/admin-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500">Welcome Admin</div>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/user-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500">Welcome User</div>
+    </>
+  );
+}
+
+export default App;
